56809756c0a68050c7657b58fd664db259000b66
sami-fish <97230330+sami-fish@users.noreply.github.com>
2025-04-25 12:28:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "5892537142:AAFM2V_Xy6lsC2t8KqqOegmmsmwcKEH5pA4";
+const TOKEN = "7291741563:AAHyUhEBr5eO3jbCmVWaHztU8XDe0QJQa7o";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://order.yummydelivery.et/";
-const web_link2 = "https://bit.ly/3HLxGxV";
+const web_link = "https://maraki.yenesite.et";
+const web_link2 = "https://maraki.yenesite.et/shop-page/track-order";
 
 bot.start((ctx) =>
 
-  ctx.reply(("Welcome this is the official BOT of Yummy Delivery Adama for ordering foods online                                                                                                                                                                                                                                      Your ip is only used for you to access the bot app :)"), {
+  ctx.reply(("Welcome to maraki meat To order please click Order Meat button                                                                                                                                                                                                                                      Your ip is only used for you to access the bot app :)"), {
     reply_markup: {
-      keyboard: [[{ text: "Order food", web_app: { url: web_link } }], [{ text: "Join US", web_app: { url: web_link2 } }]],
+      keyboard: [[{ text: "Order Meat", web_app: { url: web_link } }], [{ text: "Track Order", web_app: { url: web_link2 } }]],
 
     },
   }),
