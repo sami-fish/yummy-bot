52054fe7ec5458a4f54b8ac5fb43e2b9e9f3d626
sami-fish <97230330+sami-fish@users.noreply.github.com>
2025-04-25 13:03:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,10 @@ const bot = new Telegraf(TOKEN);
 
 const web_link = "https://maraki.yenesite.et";
 const web_link2 = "https://maraki.yenesite.et/shop-page/track-order";
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+});
 const PORT = process.env.PORT || 3000;
 
 bot.start((ctx) =>
